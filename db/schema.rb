5f12c4d5f0cf11df46d61dda86ffdb8f632beaf2
Maktech07 <maktech07@gmail.com>
2015-04-18 23:08:49 -0500

    email resource added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419015711) do
+ActiveRecord::Schema.define(version: 20150419035758) do
 
   create_table "address_people", force: true do |t|
     t.integer  "person_id"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20150419015711) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "emails", force: true do |t|
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
