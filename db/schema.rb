a8b045f349c2acd90307be1f0c6faf343ca0632f
Maktech07 <maktech07@gmail.com>
2015-04-24 08:31:34 -0500

    Adding Department resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424132429) do
+ActiveRecord::Schema.define(version: 20150424132815) do
 
   create_table "address_people", force: true do |t|
     t.integer  "person_id"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20150424132429) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "departments", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
